88e5f1d595df5b6c4308d8911f86e482782736b3
unknown <indahaulia019@gmail.com>
2022-01-08 09:58:28 +0700

    update tugas

--- SQLite_11684/app/src/main/java/com/udinus/sqlite_11684/MainActivity.java
+++ SQLite_11684/app/src/main/java/com/udinus/sqlite_11684/MainActivity.java
@@ -10,60 +10,41 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
     EditText xnim;
     EditText xnama;
     Button tblAdd;
     Button tblView;
-    DatabaseHelper BantuDB;
+    DatabaseReference reff;
+    Siswa siswa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BantuDB = new DatabaseHelper(this);
+
         xnim = (EditText)findViewById(R.id.xnim);
         xnama = (EditText)findViewById(R.id.xnama);
         tblAdd = (Button)findViewById(R.id.tblAdd);
         tblView = (Button)findViewById(R.id.tblView);
-        viewAll();
+        siswa = new Siswa();
+        reff = FirebaseDatabase.getInstance().getReference().child("Siswa");
+
         tblAdd.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick (View view){
-                boolean isInterted =
-                        BantuDB.insertData(xnim.getText().toString(),xnama.getText().toString());
-                if (isInterted == true)
-                    Toast.makeText(MainActivity.this, "Data Tersimpan", Toast.LENGTH_LONG).show();
-                else
-                    Toast.makeText(MainActivity.this, "Data Gagal Tersimpan", Toast.LENGTH_LONG).show();
+                siswa.Setnim(xnim.getText().toString().trim());
+                siswa.SetNama_siswa(xnama.getText().toString().trim());
+                    reff.push().setValue(siswa);
+                        Toast.makeText( MainActivity.this, "Data Tersimpan", Toast.LENGTH_LONG).show();
+
             }
 
         });
-    }
-
-    public void viewAll(){
-        tblView.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick (View v){
-                        Cursor res = BantuDB.getAllData();
-                        if (res.getCount() == 0){
-                            //show message
-                            showMessage ("Error", "Nothing Found");
-                            return;
-                        }
-                        StringBuffer buffer = new StringBuffer();
-                        while (res.moveToNext()){
-                            buffer.append("Nim :"+ res.getString(0)+"\n");
-                            buffer.append("Nama Mahasiswa :"+res.getString(1)+"\n");
-                        }
-
-                        //show All Data
-                        showMessage ("Mahasiswa :", buffer.toString());
-
-                    }
-                });
     }
 
     public void showMessage(String title, String Message){
